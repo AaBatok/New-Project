0b8a99a3e95858bb63c21617416bbe33d7c5fae6
AaBatok <batokcrypto@gmail.com>
2025-07-02 18:25:17 +0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from Python!")
+
+if __name__ == "__main__":
+    main()
+print("Commit ke-4 dari AaBatok")
+print("Ini commit ke-5 dari AaBatok")
+print("Ini commit ke-6 dari AaBatok")
